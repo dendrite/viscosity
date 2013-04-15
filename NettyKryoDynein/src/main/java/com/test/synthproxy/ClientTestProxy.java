d4095dcaa5af47a4dfdb9ed7be7721683bdb42c2
YourFullName <konilovsky@gmail.com>
2013-04-15 12:25:10 +0400

    Clean up...

--- NettyKryoDynein/src/main/java/com/test/synthproxy/ClientTestProxy.java
+++ NettyKryoDynein/src/main/java/com/test/synthproxy/ClientTestProxy.java
@@ -17,12 +17,6 @@ public class ClientTestProxy {
     public static void main(String... args){
     ClassLoader classLoader = IPCity.class.getClassLoader();
 
-        IPCity c = new PCity();
-
-        IPCity proxy = (IPCity) Proxy.newProxyInstance(classLoader,new Class[]{IPCity.class},new CityHandler(c));
-
-        System.out.println(proxy.createString("FFF"));
-
         IPCity proxyRemote = (IPCity) ProxyFactory.getInstance().newProxyInstance(IPCity.class);
         System.out.println("Remote server response:" + proxyRemote.createString("EEE"));
 
