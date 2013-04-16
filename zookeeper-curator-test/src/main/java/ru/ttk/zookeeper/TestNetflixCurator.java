62e7038b8e1d89a56a233d850d1d8252ec4d85a4
YourFullName <konilovsky@gmail.com>
2013-04-16 15:47:27 +0400

    Zookeeper curator Netflix test

--- zookeeper-curator-test/src/main/java/ru/ttk/zookeeper/TestNetflixCurator.java
+++ zookeeper-curator-test/src/main/java/ru/ttk/zookeeper/TestNetflixCurator.java
@@ -1,5 +1,13 @@
 package ru.ttk.zookeeper;
 
+import com.google.common.base.Throwables;
+import com.netflix.curator.framework.CuratorFramework;
+import com.netflix.curator.framework.CuratorFrameworkFactory;
+import com.netflix.curator.retry.ExponentialBackoffRetry;
+import com.netflix.curator.retry.RetryNTimes;
+import org.apache.zookeeper.CreateMode;
+
+import java.io.IOException;
 import java.io.Serializable;
 
 /**
@@ -11,7 +19,47 @@ import java.io.Serializable;
  */
 public class TestNetflixCurator implements Serializable {
 
-    public static void main(String... args) {
+    public static void main(String... args) throws Exception {
+
+//        String connectionString = "127.0.0.1:2128";
+//
+//        CuratorFramework client = null;
+//
+//            client = CuratorFrameworkFactory.builder()
+//                    .connectionTimeoutMs(1000)
+//                    .retryPolicy(new RetryNTimes(10, 500))
+//                    .connectString(connectionString)
+//                    .build();
+//
+//        client.start();
+
+        // these are reasonable arguments for the ExponentialBackoffRetry. The first
+        // retry will wait 1 second - the second will wait up to 2 seconds - the
+        // third will wait up to 4 seconds.
+
+        String connectionString = "localhost:2181";
+        CuratorFramework client = CuratorFrameworkFactory.newClient(connectionString, new ExponentialBackoffRetry(500, 3));
+
+
+        client.start();
+
+
+        System.out.println(client.getNamespace());
+
+        String path = "/zookeeper/testNode_EPH_" + System.currentTimeMillis();
+        String value = "simple data";
+
+        //client.create().forPath(path, value.getBytes());
+        client.create().withMode(CreateMode.EPHEMERAL).forPath(path, value.getBytes());
+
+
+
+        byte[] some = client.getData().forPath(path);
+        System.out.println("Get back is:" + new String(some));
+
+
+        Thread.sleep(30000);
+        client.close();
 
     }
 
