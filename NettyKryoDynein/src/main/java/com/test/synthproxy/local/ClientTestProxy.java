521ae0a607c64aced7f78c20d0ae5915ae8dcfcf
dendrite <konilovsky@gmail.com>
2013-04-24 00:48:55 +0400

    Done remote server & client communication renamed

--- NettyKryoDynein/src/main/java/com/test/synthproxy/local/ClientTestProxy.java
+++ NettyKryoDynein/src/main/java/com/test/synthproxy/local/ClientTestProxy.java
@@ -11,11 +11,9 @@ import com.test.synthproxy.shared.PClient;
 public class ClientTestProxy {
 
     public static void main(String... args){
-        ClassLoader classLoader = IPCity.class.getClassLoader();
 
         IPCity proxy = (IPCity) ProxyFactory.getInstance().newProxyInstance(IPCity.class);
         System.out.println("Remote server response:" + proxy.createString("EEE"));
-
 
         PClient client = proxy.getClientForAddress("SIMPLE ADDRESS");
         System.out.println("Client:" + client);
