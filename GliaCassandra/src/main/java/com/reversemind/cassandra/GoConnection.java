b4bc7b06dba46c81b92f39f6378a1c8035093d9a
Konilovsky <konilovsky@gmail.com>
2013-05-07 11:41:46 +0400

    First Netflix Cassandra

---
+++ GliaCassandra/src/main/java/com/reversemind/cassandra/GoConnection.java
@@ -0,0 +1,20 @@
+package com.reversemind.cassandra;
+
+import java.io.Serializable;
+
+/**
+ * Date: 5/7/13
+ * Time: 11:40 AM
+ *
+ * @author konilovsky
+ * @since 1.0
+ */
+public class GoConnection implements Serializable {
+
+    public static void main(String... args) {
+
+
+
+    }
+
+}
