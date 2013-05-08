e7caaec98feb55bea4990b3652b17d2716f7bde0
Konilovsky <konilovsky@gmail.com>
2013-05-08 17:39:34 +0400

    Test from Kundera Cassandra - is OK - what about selects??

--- GliaCassandra/src/main/java/com/reversemind/kundera/KunderaExample.java
+++ GliaCassandra/src/main/java/com/reversemind/kundera/KunderaExample.java
@@ -6,11 +6,12 @@ import javax.persistence.Persistence;
 
 public class KunderaExample {
     public static void main(String[] args) {
+
         User user = new User();
-        user.setUserId("0001");
-        user.setFirstName("John");
-        user.setLastName("Smith");
-        user.setCity("London");
+        user.setUserId("0002");
+        user.setFirstName("John2");
+        user.setLastName("Smith2");
+        user.setCity("London2");
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("cassandra_pu");
         EntityManager em = emf.createEntityManager();
@@ -18,5 +19,6 @@ public class KunderaExample {
         em.persist(user);
         em.close();
         emf.close();
+
     }
 }
