58d8f0a83ff53d90b81d1d002166e5bc1d31e502
Eugene Kalinin <kalinin.eugene@gmail.com>
2013-11-15 16:11:04 +0400

    Issue #9 Migrate to SLF4J logging + Licence

--- Glia/glia-examples/src/test/java/com/reversemind/glia/other/future/FutureTask_TimeOut_Example.java
+++ Glia/glia-examples/src/test/java/com/reversemind/glia/other/future/FutureTask_TimeOut_Example.java
@@ -10,11 +10,20 @@ import java.io.Serializable;
 import java.util.concurrent.*;
 
 /**
- * Date: 4/26/13
- * Time: 11:51 AM
  *
- * @author konilovsky
- * @since 1.0
+ * Copyright (c) 2013 Eugene Kalinin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 public class FutureTask_TimeOut_Example implements Serializable {
 
